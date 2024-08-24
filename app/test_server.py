a569926539fb83d302672133c2f711e28c2c7c88
imoutidi <imoutidi@gmail.com>
2024-08-24 23:40:28 +0200

    Request test for the API.

---
+++ app/test_server.py
@@ -0,0 +1,6 @@
+import requests
+from requests.auth import HTTPBasicAuth
+
+print(requests.post("http://127.0.0.1:8000/sentence_sentiment",
+                    json={"text": "the application was <blank>"},
+                    auth=HTTPBasicAuth("user", "31415")).json())
